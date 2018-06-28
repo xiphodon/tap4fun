b70c33a3f8fc59d96da0e9170e6aedb254edb450
GuoChang <827789895@qq.com>
2018-06-28 17:25:40 +0800

    tap4fun

---
+++ demo.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/6/28 17:04
+# @Author  : GuoChang
+# @Site    : https://github.com/xiphodon
+# @File    : demo.py
+# @Software: PyCharm
+
+import os
+import numpy as np
+import pandas as pd
+
+data_dir_path = r'E:\work_all\my_data\tap4fun\input'
+
+
+def read_test_data():
+    """
+    读取测试数据集
+    :return:
+    """
+
+
+def read_train_data():
+    """
+    读取训练数据集
+    :return:
+    """
+
+
+def create_sample_data():
+    """
+    创建数据集样本
+    :return:
+    """
+
+
+def start():
+    """
+    启动入口
+    :return:
+    """
+    create_sample_data()
+
+
+if __name__ == '__main__':
+    start()
